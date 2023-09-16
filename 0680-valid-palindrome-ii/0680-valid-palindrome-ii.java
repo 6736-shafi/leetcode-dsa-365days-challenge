97a2f7d407537e8a773eeaed8382693224f1315f
MD SHAFI UDDIN <76084009+6736-shafi@users.noreply.github.com>
2023-09-16 23:18:38 +0530

    Time: 4 ms (100%), Space: 44.5 MB (27.43%) - LeetHub

---
+++ 0680-valid-palindrome-ii/0680-valid-palindrome-ii.java
@@ -0,0 +1,30 @@
+class Solution {
+    public boolean validPalindrome(String s) {
+        int a_pointer=0;
+        int b_pointer=s.length()-1;
+        while(a_pointer<=b_pointer){
+            if(s.charAt(a_pointer)!=s.charAt(b_pointer)){
+                return helper(s,a_pointer+1,b_pointer) || helper(s,a_pointer,b_pointer-1) ;
+            }
+            a_pointer++;
+            b_pointer--;
+        }
+
+        return true;
+        
+    }
+     public boolean helper(String s,int i,int j) {
+        int a_pointer=i;
+        int b_pointer=j;
+        while(a_pointer<=b_pointer){
+            if(s.charAt(a_pointer)!=s.charAt(b_pointer)){
+                return false ;
+            }
+            a_pointer++;
+            b_pointer--;
+        }
+
+        return true;
+        
+    }
+}
